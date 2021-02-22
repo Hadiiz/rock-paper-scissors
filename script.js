8d788c97c636451b3dbaa7f4da48ade32fddb248
Hadiiz <hadiziadeh@hotmail.com>
2021-02-22 23:26:26 +0200

    Fixed Script + DOM

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+const choices = document.querySelectorAll('.choice');
+const prompt = document.getElementById('prompt');
+const score = document.getElementById('score');
+const scoreboard = {
+    player: 0,
+    computer: 0
+};
+
+// Event Listeners
+choices.forEach(choice => choice.addEventListener('click', play));
+
 function computerPlay(){
     let value = Math.floor(Math.random()*3)+1;
     switch(value){
         case 1: 
             return "rock";
-            break;
         case 2:
             return "paper";
-            break;
         default:
             return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection){
+    
     if(playerSelection == computerSelection)
         return "Tie!";
     else if(playerSelection === "rock" && computerSelection === "paper")
@@ -31,43 +41,15 @@ function playRound(playerSelection, computerSelection){
         console.log("WRONG INPUT!");
 }
 
-function game(){
-    let playerSelection;
-    let computerSelection;
-    let result;
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while (playerScore <5 && computerScore<5){
-        playerSelection =  prompt("Your Turn!: ");
-        playerSelection = playerSelection.toLowerCase();
-        computerSelection =  computerPlay();
-        result = playRound(playerSelection, computerSelection);
-        if(result[4] == "L"){
-            computerScore ++;
-            console.log("Computer Wins this Round!");
-        }
-        else if(result[4] == "W"){
-            playerScore ++;
-            console.log("Player Wins this Round!");
-        }
-        else{
-            console.log("Tie!");
-        }
-    }
-
-    console.log("GAME OVER! "+playerScore<5? "Computer":"Player"+" Wins!");
+function play(e){
+    const playerSelection = e.target.id;
+    const computerSelection = computerPlay();
+    const roundResult = playRound(playerSelection, computerSelection);
+    if(roundResult[4] == "W")
+        scoreboard.player++;
+    else if(roundResult[4] == "L")
+        scoreboard.computer++;
+    prompt.textContent = roundResult;
+    score.textContent = `${scoreboard.player} - ${scoreboard.computer}`;
+    
 }
-
-const rock = document.getElementById('rock');
-rock.addEventListener('click', ()=>{
-    alert("rock");
-});
-const paper = document.getElementById('paper');
-paper.addEventListener('click', ()=>{
-    alert("paper");
-});
-const scissors = document.getElementById('scissors');
-scissors.addEventListener('click', ()=>{
-    alert("scissors");
-});
